43a7f5018c724c418191b97a21bf0b64c0ee3fd2
Junior580 <junior_silvapereira@hotmail.com>
2023-02-11 02:02:56 -0300

    ++ test

--- src/modules/users/services/GetUserService.spec.ts
+++ src/modules/users/services/GetUserService.spec.ts
@@ -12,34 +12,22 @@ describe('Get users', () => {
     fakeUsersRepository = new UserRepositoryInMemory()
     getUser = new GetUserService(fakeUsersRepository)
     createUser = new CreateUserService(fakeUsersRepository)
+  })
 
-    const user = await createUser.execute({
+  it('should be able to get users', async () => {
+    const userCreated = await createUser.execute({
       name: 'user1',
       email: 'user1@email.com',
       password: '123456',
     })
-  })
 
-  it('should be able to get users', async () => {
     const user = await getUser.execute()
 
     expect(user[0]).toHaveProperty('id')
   })
+
   it('should be not able to return user if the list is empty', async () => {
-    const user = await createUser.execute({
-      name: 'user1',
-      email: 'user1@email.com',
-      password: '123456',
-    })
-
-    expect(user).toHaveProperty('id')
-
-    await expect(
-      createUser.execute({
-        name: 'user1',
-        email: 'user1@email.com',
-        password: '123456',
-      })
-    ).rejects.toBeInstanceOf(AppError)
+    const user = await getUser.execute()
+    await expect(user.length === 0).rejects.toBeInstanceOf(AppError)
   })
 })
