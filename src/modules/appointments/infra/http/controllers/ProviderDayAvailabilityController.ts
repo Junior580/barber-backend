1f34d2f69cd4c948edefb7814b6e4ad3a5aabe9a
Junior580 <junior_silvapereira@hotmail.com>
2023-02-21 14:49:20 -0300

    feat: adding controller provider day/month avai..

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import { ListProviderDayAvailabilityService } from '@modules/appointments/services/ListProviderDayAvailabilityService'
+import { AppointmentsRepository } from '../../typeorm/repositories/AppointmentsRepository'
+
+export class ProviderDayAvailabilityController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params
+    const { month, year, day } = request.body
+
+    const appointmentRepo = new AppointmentsRepository()
+
+    const listDayProvider = new ListProviderDayAvailabilityService(
+      appointmentRepo
+    )
+
+    const providers = await listDayProvider.execute({
+      provider_id,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
+    })
+
+    return response.json(providers)
+  }
+}
---
+++ src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { ListProviderMonthAvailabilityService } from '@modules/appointments/services/ListProviderMonthAvailabilityService'
+import { AppointmentsRepository } from '../../typeorm/repositories/AppointmentsRepository'
+
+export class ProviderMonthAvailabilityController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params
+    const { month, year } = request.body
+
+    const appointmentRepo = new AppointmentsRepository()
+
+    const listMonthProvider = new ListProviderMonthAvailabilityService(
+      appointmentRepo
+    )
+
+    const providers = await listMonthProvider.execute({
+      provider_id,
+      month: Number(month),
+      year: Number(year),
+    })
+
+    return response.json(providers)
+  }
+}
