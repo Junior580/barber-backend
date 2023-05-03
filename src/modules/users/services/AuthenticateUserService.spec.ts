c9746435b7c81c23a9fea086bbc0f60106be4718
Junior580 <junior_silvapereira@hotmail.com>
2023-05-02 23:58:20 -0300

    add workflow for test coverage 1.7

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,70 +1,70 @@
-// import AppError from '../../../shared/errors/AppError'
-// import { InMemoryUserRepository } from '../repositories/inMemory/InMemoryUserRepository'
-// import { CreateUserService } from '../services/CreateUserService'
-// import { AuthenticateUserService } from './AuthenticateUserService'
-// import { InMemoryHashProvider } from '../providers/HashProvider/inMemory/InMemoryHashProvider'
-// import { InMemoryCacheProvider } from '@shared/container/providers/CacheProvider/inMemory/InMemoryCacheProvider'
+import AppError from '../../../shared/errors/AppError'
+import { InMemoryUserRepository } from '../repositories/inMemory/InMemoryUserRepository'
+import { CreateUserService } from '../services/CreateUserService'
+import { AuthenticateUserService } from './AuthenticateUserService'
+import { InMemoryHashProvider } from '../providers/HashProvider/inMemory/InMemoryHashProvider'
+import { InMemoryCacheProvider } from '@shared/container/providers/CacheProvider/inMemory/InMemoryCacheProvider'
 
-// let fakeUsersRepository: InMemoryUserRepository
-// let createUser: CreateUserService
-// let authenticateUser: AuthenticateUserService
-// let inMemoryHashProvider: InMemoryHashProvider
-// let memoryCacheProvider: InMemoryCacheProvider
+let fakeUsersRepository: InMemoryUserRepository
+let createUser: CreateUserService
+let authenticateUser: AuthenticateUserService
+let inMemoryHashProvider: InMemoryHashProvider
+let memoryCacheProvider: InMemoryCacheProvider
 
-// describe('Create User', () => {
-//   beforeEach(() => {
-//     fakeUsersRepository = new InMemoryUserRepository()
-//     inMemoryHashProvider = new InMemoryHashProvider()
-//     memoryCacheProvider = new InMemoryCacheProvider()
+describe('Create User', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new InMemoryUserRepository()
+    inMemoryHashProvider = new InMemoryHashProvider()
+    memoryCacheProvider = new InMemoryCacheProvider()
 
-//     createUser = new CreateUserService(
-//       fakeUsersRepository,
-//       inMemoryHashProvider,
-//       memoryCacheProvider
-//     )
-//     authenticateUser = new AuthenticateUserService(
-//       fakeUsersRepository,
-//       inMemoryHashProvider
-//     )
-//   })
+    createUser = new CreateUserService(
+      fakeUsersRepository,
+      inMemoryHashProvider,
+      memoryCacheProvider
+    )
+    authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      inMemoryHashProvider
+    )
+  })
 
-//   it('should be able to authenticate', async () => {
-//     const user = await createUser.execute({
-//       name: 'user1',
-//       email: 'user1@email.com',
-//       password: '123456',
-//     })
+  it('should be able to authenticate', async () => {
+    const user = await createUser.execute({
+      name: 'user1',
+      email: 'user1@email.com',
+      password: '123456',
+    })
 
-//     const response = await authenticateUser.execute({
-//       email: 'user1@email.com',
-//       password: '123456',
-//     })
+    const response = await authenticateUser.execute({
+      email: 'user1@email.com',
+      password: '123456',
+    })
 
-//     expect(response).toHaveProperty('token')
-//     expect(response.user).toEqual(user)
-//   })
+    expect(response).toHaveProperty('token')
+    expect(response.user).toEqual(user)
+  })
 
-//   it('should not authenticate with a non existing user', async () => {
-//     await expect(
-//       authenticateUser.execute({
-//         email: 'user1@email.com',
-//         password: '123456',
-//       })
-//     ).rejects.toBeInstanceOf(AppError)
-//   })
+  it('should not authenticate with a non existing user', async () => {
+    await expect(
+      authenticateUser.execute({
+        email: 'user1@email.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
 
-//   it('should not be able to authenticate with wrong password', async () => {
-//     await createUser.execute({
-//       name: 'user1',
-//       email: 'user1@email.com',
-//       password: '123456',
-//     })
+  it('should not be able to authenticate with wrong password', async () => {
+    await createUser.execute({
+      name: 'user1',
+      email: 'user1@email.com',
+      password: '123456',
+    })
 
-//     await expect(
-//       authenticateUser.execute({
-//         email: 'user1@email.com',
-//         password: 'wrongPass',
-//       })
-//     ).rejects.toBeInstanceOf(AppError)
-//   })
-// })
+    await expect(
+      authenticateUser.execute({
+        email: 'user1@email.com',
+        password: 'wrongPass',
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
