e9f5db948aa1e4b4e5db632d866cbe5cbfc76b71
Junior580 <junior_silvapereira@hotmail.com>
2023-05-19 23:03:40 -0300

    feat: ++errros, middlewares erros

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -39,7 +39,7 @@ export class AuthenticateUserService {
 
     const { secret, expiresIn } = jwt
 
-    const token = sign({}, `${process.env.JWT_PASS}`, {
+    const token = sign({}, secret, {
       subject: user.id,
       expiresIn,
     })
